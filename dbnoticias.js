8d3aec864a4a708da897066c998935af021787ed
FerrazAndre <102933178+AndreFerrazzz@users.noreply.github.com>
2022-06-12 23:37:29 -0300

    Add files via upload

---
+++ dbnoticias.js
@@ -0,0 +1,29 @@
+var db = {
+    dados:[
+        {
+            titulo: 'Samu orienta 115 moradores de morros em Santos sobre primeiros socorros',
+            referencia: 'Prefeitura de Santos',
+            corpo: 'O intuito foi capacitar e aproximar os moradores, principalmente de áreas de difícil acesso para as ambulâncias. Eles foram orientados sobre como agir nos principais casos de emergência de saúde e receberam orientações a respeito do funcionamento dos serviços do Samu.',
+            datas: '12/05/2022',
+            //url: 'https://www.santos.sp.gov.br/?q=noticia/samu-orienta-115-moradores-de-morros-em-santos-sobre-primeiros-socorros'
+        }, {
+            titulo: 'Confira 12 itens de primeiros socorros que você deve ter em casa',
+            referencia: 'Jornal Metrópoles',
+            corpo: 'Alguns produtos não costumam estar nos kits que as pessoas têm em casa, mas valem o investimento. Um abaixador de língua, por exemplo, pode ajudar a ver algum objeto na garganta e até a imobilizar um dedo da mão. Outra boa aquisição é uma bolsa térmica, que pode ser usada tanto fria quanto quente e serve para cólicas ou mesmo hematomas ou inchaços por pancadas.',
+            datas: '16/05/2022',
+           // url: 'https://www.metropoles.com/saude/confira-12-itens-de-primeiros-socorros-que-voce-deve-ter-em-casa'
+    }, {
+            titulo: 'iFood e Prefeitura realizam capacitação em primeiros socorros para entregadores de Salvador',
+            referencia: 'Jornal Correio',
+            corpo: 'A capacitação ocorreu no Salvador Shopping, no Instituto JCPM de Compromisso Social (Espaço IJCPM), como parte das ações de conscientização que marcam o Maio Amarelo.',
+            datas: '27/05/2022',
+           //URL: 'https://www.correio24horas.com.br/noticia/nid/ifood-e-prefeitura-realizam-capacitacao-em-primeiros-socorros-para-entregadores-de-salvador/'
+    },{
+            titulo: 'Criança de um ano e meio se engasga com maçã e é salva por policial em Maracanaú',
+            referencia: 'Jornal O Povo',
+            corpo: 'Criança voltou a respirar após PM aplicar a manobra de Heimlich, técnica de primeiros socorros recomendada em casos de obstrução das vias respiratórias',
+            datas: '05/06/2022',
+           // url: 'https://www.opovo.com.br/noticias/ceara/maracanau/2022/06/05/crianca-de-um-ano-e-meio-se-engasga-com-maca-e-e-salva-por-policial-em-maracanau.html'
+    }
+    ]
+}
